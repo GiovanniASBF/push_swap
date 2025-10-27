c2a174f768faa360877903e52043e2f8be48dec5
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-10-27 17:32:13 -0300

    feat: add the first int validation
    Check if the arguments are integers only

---
+++ to_deliver/ft_push_swap.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_swap.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 14:19:55 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/10/27 16:20:33 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_push_swap.h"
+
+int	main(int argc, char*argv[])
+{
+	int	i;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			// verificar se são inteiros validos [usar o isdigit()]
+			// verificar se não é maior ou menor que o maxint e minint
+			/*
+			verificar se não há duplicatas
+			--
+			criar um indice 
+			a cada valor percorrido, adicionar ele ao indice
+			para cada novo valor, percorrer o indice e ver se esse valor já foi colocado lá
+			*/
+		}
+		
+	}
+	else
+		exit(1);
+}
