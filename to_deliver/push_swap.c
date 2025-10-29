c5512f4d54aece92a30eaa8c8a5ee001225b66bf
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-10-29 10:31:24 -0300

    add Makefile

--- to_deliver/push_swap.c
+++ to_deliver/push_swap.c
@@ -6,11 +6,12 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 14:19:55 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/10/28 18:12:53 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:18:31 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	main(int argc, char*argv[])
 {
@@ -29,14 +30,18 @@ int	main(int argc, char*argv[])
 		temp = str;
 		str = ft_strjoin(temp, " ");
 		free(temp);
-		// verificar se são inteiros validos [usar o isdigit()]
-		// verificar se não é maior ou menor que o maxint e minint
-		/*
+	}
+
+	printf("%s\n", str);
+}
+
+/*
+verificar se são inteiros validos [usar o isdigit()]
+		verificar se não é maior ou menor que o maxint e minint
+		
 		verificar se não há duplicatas
 		--
 		criar um indice 
 		a cada valor percorrido, adicionar ele ao indice
 		para cada novo valor, percorrer o indice e ver se esse valor já foi colocado lá
-		*/
-	}
-}
+*/
