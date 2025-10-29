87dd54a25de0491ba6e33c74e6377ef6d2ce172a
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-10-29 17:06:31 -0300

    add dealing with integer limit

--- to_deliver/parser.c
+++ to_deliver/parser.c
@@ -6,7 +6,7 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 16:26:35 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/10/29 16:59:25 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/10/29 17:05:02 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	parse_and_validate(t_list	**stack_a, int argc, char*argv[])
 {
 	char	**arglist;
 	int		i;
+	long	n_long;
+	t_list	*node;
 
 	arglist = argument_unifier(argv);
 	if (!arglist || !only_digits(arglist))
@@ -23,6 +25,9 @@ void	parse_and_validate(t_list	**stack_a, int argc, char*argv[])
 	i = 0;
 	while (arglist[i])
 	{
+		n_long = ft_atol(arglist[i]);
+		if ((n_long > 2147483647) || (n_long < -2147483648))
+			handle_error(stack_a, arglist);
 		printf("%s ", arglist[i]);
 		i++;
 	}
