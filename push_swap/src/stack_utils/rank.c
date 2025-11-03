a1f777ec467a61410c9bd715ddfc821a86a72227
GiovanniBernardesFigueiredo <giovannibernardesfigueiredo@gmail.com>
2025-11-03 10:48:31 -0300

    src/stack_utils refactored
    divide list_handler into 2 classes with better names

---
+++ push_swap/src/stack_utils/rank.c
@@ -0,0 +1,60 @@
+#include "push_swap.h"
+
+t_list	*lstdup(t_list *head)
+{
+	t_list	*current;
+	t_list	*new_head;
+	t_list	*new_node;
+
+	new_head = NULL;
+	current = head;
+	while (current)
+	{
+		new_node = fill_new_node(*(int *)(current->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_head, &free);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_head, new_node);
+		current = current-> next;
+	}
+	return (new_head);
+}
+
+int	index_list(t_list *iter_node, int refer_val, int lst_index)
+{
+	while (iter_node)
+	{
+		if (refer_val > *(int *)(iter_node->content))
+			lst_index++;
+		iter_node = iter_node->next;
+	}
+	return (lst_index);
+}
+
+void	index_manager(t_list **head_ptr)
+{
+	t_list	*copy_head;
+	t_list	*iterator_node;
+	t_list	*reference_node;
+	int		refer_val;
+	int		lst_index;
+
+	if (!head_ptr || !*head_ptr)
+		return ;
+	copy_head = lstdup(*head_ptr);
+	if (!copy_head)
+		handle_error(head_ptr, NULL);
+	reference_node = *head_ptr;
+	while (reference_node)
+	{
+		refer_val = *(int *)(reference_node->content);
+		lst_index = 0;
+		iterator_node = copy_head;
+		lst_index = index_list(iterator_node, refer_val, lst_index);
+		*(int *)(reference_node->content) = lst_index;
+		reference_node = reference_node->next;
+	}
+	ft_lstclear(&copy_head, &free);
+}
